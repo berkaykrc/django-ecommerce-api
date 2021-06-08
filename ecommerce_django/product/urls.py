63d3eb462b8e25700b8a71e6866a320a42b7dab2
berkaykrc <berkay.kirci@gmail.com>
2021-06-08 22:42:15 +0300

    final commit

---
+++ ecommerce_django/product/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from product import views
+
+urlpatterns = [
+
+    path('latest-products/', views.LastestProductsList.as_view()),
+    path('product/search/', views.search),
+    path('product/<slug:category_slug>/<slug:product_slug>/',
+         views.ProductDetail.as_view()),
+    path('product/<slug:category_slug>/', views.CategoryDetail.as_view())
+
+]
